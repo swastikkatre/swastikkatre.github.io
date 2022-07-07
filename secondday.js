fade31588519a4b27ef3845893d2a3ec54a3d7bd
swastikkatre <108858223+swastikkatre@users.noreply.github.com>
2022-07-07 23:09:32 +0530

    Add files via upload

--- secondday.js
+++ secondday.js
@@ -1,6 +1,10 @@
 function add(){
 	var a=document.getElementById('n1').value;
-	var b=a*2;
-	console.log(b)
-	document.getElementById('n2').value=b;
+	var b=document.getElementById('n2').value;
+	document.getElementById('n3').value=parseInt(a)+parseInt(b);
+}
+function subtract(){
+	var a=document.getElementById('n1').value;
+	var b=document.getElementById('n2').value;
+	document.getElementById('n3').value=parseInt(a)-parseInt(b);
 }
