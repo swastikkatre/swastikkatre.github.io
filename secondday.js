1ae8b8e764ebaa2ad978cef1ca6cdffd0a99b2f9
swastikkatre <108858223+swastikkatre@users.noreply.github.com>
2022-07-07 23:31:36 +0530

    added multiple division function

--- secondday.js
+++ secondday.js
@@ -8,3 +8,15 @@ function subtract(){
 	var b=document.getElementById('n2').value;
 	document.getElementById('n3').value=parseInt(a)-parseInt(b);
 }
+function multiply(){
+	var a=document.getElementById('n1').value;
+	var b=document.getElementById('n2').value;
+	document.getElementById('n3').value=parseInt(a)*parseInt(b);
+	
+}
+function divide(){
+	var a=document.getElementById('n1').value;
+	var b=document.getElementById('n2').value;
+	document.getElementById('n3').value=parseInt(a)/parseInt(b);
+	
+}
