cab0344ef8ef64b92564c57c17097fb126f2ba0c
swastikkatre <108858223+swastikkatre@users.noreply.github.com>
2022-07-11 09:40:26 +0530

    compound intrest

--- ci.js
+++ ci.js
@@ -3,5 +3,5 @@ function ci(){
 	var n=document.getElementById('s2').value;
 	var r=document.getElementById('s3').value;
 	var a= parseInt(p)*(1+((parseInt(r))/100))*parseInt(n);
-            document.getElementById('S4').value =a;
+            document.getElementById('s4').value =a;
 }
