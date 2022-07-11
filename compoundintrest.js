baa53b5a0149acd789f6201e21ba6fdc89f2c0a9
swastikkatre <108858223+swastikkatre@users.noreply.github.com>
2022-07-11 09:46:59 +0530

    compound intrest

--- compoundintrest.js
+++ compoundintrest.js
@@ -2,7 +2,7 @@ function compoundintrest(){
 	var p=document.getElementById('s1').value;
 	var n=document.getElementById('s2').value;
 	var r=document.getElementById('s3').value;
-	var a=p*n*r;
+	var a=parseInt(p)*(1+((parseInt(r))/100))*parseInt(n);
 	
 	document.getElementById('s4').value =a;
 	
