633b0b56ec11a0a01d3bfee6206f98048a954d24
swastikkatre <108858223+swastikkatre@users.noreply.github.com>
2022-07-11 09:43:22 +0530

    compoundintrest

---
+++ compoundintrest.js
@@ -0,0 +1,9 @@
+function compoundintrest(){
+	var p=document.getElementById('s1').value;
+	var n=document.getElementById('s2').value;
+	var r=document.getElementById('s3').value;
+	var a=p*n*r;
+	
+	document.getElementById('s4').value =a;
+	
+}
